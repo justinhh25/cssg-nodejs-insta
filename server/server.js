d4d4284dc2e858564514083aa5c7a2738d10c72f
justinhh25 <justinhan2005@gmail.com>
2025-03-01 14:31:43 -0500

    setup & pages complete

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const app = express()
+app.use(express.static(path.join(__dirname, "../client/public")));
+app.use(express.json())
+
+app.get("/home", (req, res) => {
+    res.sendFile(path.join(__dirname,"../client/public/index.html"));
+});
+app.get("/profile", (req, res) => { 
+    res.sendFile(path.join(__dirname, "../client/public/profile.html")); 
+}); 
+
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
+  
